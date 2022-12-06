e6aa6ce54b98f9ab3dda8051cf48c97f1daa29a9
Mia Tanner <mia@indygecko.com>
2022-12-05 18:15:47 -0800

    make function to format the amount of spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,11 +3,23 @@ import "components/DayListItem.scss";
 import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const formatSpots= function(spots) {
+    switch (Number(spots)) {
+      case (1):
+        return "1 spot remaining";
+      case (0):
+        return "no spots remaining";
+      default:
+        return `${spots} spots remaining`;
+    }
+
+  };
+  const formattedSpots = formatSpots(props.spots);
   const dayClass = classNames("day-list__item", {"--selected": props.selected, "--full": !props.spots}).split(" ").join("");
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)} >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
