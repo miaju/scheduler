0c7ebdfc20e8b6136c478c2da0559d8cb5e949ce
Mia Tanner <mia@indygecko.com>
2022-12-05 22:07:45 -0800

    implement InterviewerList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,9 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const days = props.days.map((day) => {
     return (
-      ((props.day===day.name) ? <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay} selected/> : <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay}/>)
+      ((props.day===day.name) ? 
+      <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay} selected/> 
+      : <DayListItem key={day.id} name={day.name} spots={day.spots} setDay={props.setDay}/>)
     );
   });
   return(
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,10 +3,19 @@ import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss"
 
 export default function InterviewList(props) {
+  const interviewers = props.interviewers.map((inter) => {
+    return(
+      ((props.interviewer===inter.id) ? 
+      <InterviewerListItem key={inter.id} id={inter.id} name={inter.name} avatar={inter.avatar} setInterviewer={props.setInterviewer} selected/> 
+      : <InterviewerListItem key={inter.id} id={inter.id} name={inter.name} avatar={inter.avatar} setInterviewer={props.setInterviewer}/>)
+    );
+  });
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewers}
+      </ul>
     </section>
   );
 }
