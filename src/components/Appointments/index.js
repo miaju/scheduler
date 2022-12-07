2b73878cc1b6b3f97eb68f7156814438c54ea4ad
Mia Tanner <mia@indygecko.com>
2022-12-06 17:50:39 -0800

    create display of appointments + stories

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import "./styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
-
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} id={props.id} time={props.time} interviewer={props.interview.interviewer.name} /> : <Empty time={props.time}/>}
+    </article>
   );
 }
