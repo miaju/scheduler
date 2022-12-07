2d091fd8d9f50b2829b8ca034cc14992dd79b833
Mia Tanner <mia@indygecko.com>
2022-12-06 16:44:33 -0800

    create Appointment/Error component + stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointments/Empty";
 import Show from "components/Appointments/Show";
 import Confirm from 'components/Appointments/Confirm';
 import Status from "components/Appointments/Status";
+import Error from "components/Appointments/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -179,4 +180,5 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer="Sylvia Palmer" onDelete={action("onDelete")} onEdit={action("onEdit")}/>)
   .add("Confirm", () => <Confirm message='Delete the appointment?' onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", () => <Status message="Deleting"/>)
+  .add("Error", () => <Error message="Could not delete appointment" onClose={action("onClose")}/>)
   ;
