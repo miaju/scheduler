370654cd14975019a3b069579239216b27f2faf8
Mia Tanner <mia@indygecko.com>
2022-12-06 16:08:38 -0800

    fix storybook not applying background colors bug

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: ['../src/stories/*.js'],
   addons: [
     "@storybook/addon-actions/register",
-    "@storybook/addon-backgrounds/register",
+    "@storybook/addon-backgrounds",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,7 @@
-import './storybook.css'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
-      color: /(background|color)$/i,
       date: /Date$/,
     },
   },
