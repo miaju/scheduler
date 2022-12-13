bcd0a0309778f445d6f6e2a8e6ac95dec5648b09
Mia Tanner <mia@indygecko.com>
2022-12-12 21:32:44 -0800

    create getInterview function + tests

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -8,3 +8,14 @@ export function getAppointmentsForDay(state, dayName) {
   }
   return result;
 }
+
+export function getInterview(state, interview){
+  if (interview) {
+    return {
+      student: interview.student,
+      interviewer: state.interviewers[interview.interviewer] 
+    }
+  }
+  return null;
+
+}
