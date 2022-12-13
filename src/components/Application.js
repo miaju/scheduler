03e41feb9a155701cf963010e3bae1994e3cd4b3
Mia Tanner <mia@indygecko.com>
2022-12-12 19:45:55 -0800

    combine state for day/days/appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,60 +5,18 @@ import axios from "axios";
 import "components/Application.scss";
 import Appointment from "./Appointments";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
 export default function Application(props) {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
-  const appoins = Object.values(appointments).map((appointment) => {
-    return (<Appointment 
-      key={appointment.id} 
-      {...appointment} 
-    />)
-  })
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: []
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days").then(response => {
-      setDays(response.data)
-      console.log(response.data);
-    });
-  }, [])
+    axios.get("/api/days").then(response => setDays(response.data));
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -70,8 +28,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={days}
-        value={day}
+        days={state.days}
+        value={state.day}
         onChange={setDay}
       />
       </nav>
@@ -82,7 +40,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {appoins}
+        {state.appointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
