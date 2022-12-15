5ce8cd39d32fd5f2b596584563b3d9136f81e31c
Mia Tanner <mia@indygecko.com>
2022-12-14 21:21:05 -0800

    add confirm state to deleting appointment

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -6,12 +6,14 @@ import Show from "./Show";
 import useVisualMode from 'hooks/useVisualMode';
 import Form from './Form';
 import Status from './Status';
+import Confirm from './Confirm';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
-const DELETING = "DELETING"
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -34,18 +36,18 @@ export default function Appointment(props) {
   
   }
 
-  function del(id) {
-    transition(DELETING);
+  function confirm() {
+    transition(CONFIRM);
+  };
 
+  function del() {
+    transition(DELETING);
     const cancelInterview = async() => {
       await props.cancelInterview(props.id);
       transition(EMPTY);
     }
-
     return cancelInterview();
-  };
-
-  
+  }
 
   return (
     <article className="appointment">
@@ -56,13 +58,14 @@ export default function Appointment(props) {
           student={props.interview.student}
           interviewer={props.interview.interviewer}
           time={props.time}
-          onDelete={() => del(props.id)}
+          onDelete={() => confirm(props.id)}
         />
       )}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} time={props.time}/>}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => transition(EMPTY)} onSave={save}/>}
       {mode === SAVING && <Status message={"Saving"}/>}
       {mode === DELETING && <Status message={"Deleting"}/>}
+      {mode === CONFIRM && <Confirm message={"Are you sure you would like to delete this appointment?"} onCancel={back} onConfirm={() => del(props.id)}/>}
     </article>
   );
 }
