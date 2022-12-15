eef91d77497724fd603e99723f2ff685aa0377b4
Mia Tanner <mia@indygecko.com>
2022-12-14 16:57:50 -0800

    add promise catch for api calls

--- src/components/Application.js
+++ src/components/Application.js
@@ -40,7 +40,9 @@ export default function Application(props) {
       axios.get("/api/interviewers")
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    })
+    }).catch((error) => {
+      console.error(error.message);
+    });
   }, []);
 
   return (
