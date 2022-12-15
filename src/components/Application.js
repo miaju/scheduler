1617705c66ee18609515c3f55390b96be62ae7ba
Mia Tanner <mia@indygecko.com>
2022-12-14 17:52:08 -0800

    update api when appointment is saved

--- src/components/Application.js
+++ src/components/Application.js
@@ -29,10 +29,13 @@ export default function Application(props) {
       [id]: appointment
     };
 
-    setState({
-      ...state,
-      appointments
-    });
+    return axios.put(`api/appointments/${id}`, appointment)
+      .then(
+        setState({
+          ...state,
+          appointments
+        })
+      );
   }
 
   const schedule = appointments.map((appointment) => {
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -20,8 +20,7 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview);
-    transition(SHOW);
+    props.bookInterview(props.id, interview).then(transition(SHOW));
   }
 
   return (
