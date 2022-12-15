85dd973a19ff489420dbf2e43ef54d81c8ae0946
Mia Tanner <mia@indygecko.com>
2022-12-14 17:22:41 -0800

    make saving appointment save to state and show

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,7 +20,19 @@ export default function Application(props) {
   const interviewers = getInterviewersForDay(state, state.day);
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const schedule = appointments.map((appointment) => {
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -21,6 +21,7 @@ export default function Appointment(props) {
       interviewer
     };
     props.bookInterview(props.id, interview);
+    transition(SHOW);
   }
 
   return (
