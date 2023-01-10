b72fa24d0ec23de43e59711cdc6851712a96077e
Mia Tanner <mia@indygecko.com>
2023-01-10 09:34:09 -0800

    remove unused code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import DayList from "./DayList";
-import axios from "axios";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./helpers/selectors";
 
 import "components/Application.scss";
