989f73d40c780e22831b37e09a6267de619d0705
Mia Tanner <mia@indygecko.com>
2023-01-10 14:17:48 -0800

    add test suite for appointment component

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -73,7 +73,7 @@ export default function useApplicationData() {
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     }).catch((error) => {
-      console.error(error.message);
+      //console.error(error.message);
     });
   }, []);
 
