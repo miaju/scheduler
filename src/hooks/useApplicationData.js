1f254eaf059f3b87c4576019a3d2099cbcbe2cb1
Mia Tanner <mia@indygecko.com>
2023-01-16 19:20:57 -0800

    use mock axios library for testing

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+jest.mock('axios');
 
 export default function useApplicationData() {
 
@@ -73,7 +74,7 @@ export default function useApplicationData() {
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     }).catch((error) => {
-      //console.error(error.message);
+      console.error(error.message);
     });
   }, []);
 
