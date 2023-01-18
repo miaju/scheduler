2cdaf660645e8bbb5a57c7ce601117cf5e803778
Mia Tanner <mia@indygecko.com>
2023-01-18 15:46:01 -0800

    add cypress tests

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,56 @@
+// appointments.spec.js.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe("Appointments", () => {
+  beforeEach(() => {
+   cy.request("GET", "/api/debug/reset");
+ 
+   cy.visit("/");
+ 
+   cy.contains("Monday");
+  });
+ 
+  it("should book an interview", () => {
+   cy.get("[alt=Add]")
+    .first()
+    .click();
+ 
+   cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+   cy.get('[alt="Sylvia Palmer"]').click();
+ 
+   cy.contains("Save").click();
+ 
+   cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+   cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+  
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").click({ force: true });
+  
+    cy.contains("Confirm").click();
+  
+    cy.contains("Deleting");
+    cy.contains("Deleting").should("not.exist");
+  
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
+
+ });
