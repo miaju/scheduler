93b36b39f5742d85ca0a9e2fd189be798b5a598f
Mia Tanner <mia@indygecko.com>
2023-01-18 17:49:16 -0800

    comment explanations for useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,17 @@
 import {useState} from 'react';
 
+// Allows easy transitions between different views of the appointment component
+// takes initial: String - initial given mode
+// state {
+// mode: String,
+// history: Array
+// }
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  // sets the mode as the given newMode - if replace=true, then the last item in the history
+  // array is replaced by the new mode
   function transition(newMode, replace = false) {
     setMode(newMode);
     if (replace) {
@@ -12,6 +20,7 @@ export default function useVisualMode(initial) {
     setHistory([...history, newMode]);
     
   }
+  // removes the last item in the history array, and sets the mode to be the second to last s
   function back(){
     if (history[history.length -1] !== initial) {
       history.pop();
